aa4a24560c597c50dff518b29cc867e6f192a48a
BaNg-W <bang344251871@gmail.com>
2023-08-13 21:23:05 -0400

    Makes another request to steam for turkish prices instead of using set number for conversion so conversion isn't terrible. Also deducted an addition cent when calculating adjusted price.

--- getskinpricesteam.py
+++ getskinpricesteam.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_steam_market_price(app_id, market_hash_name):
+def get_steam_market_price_cad(app_id, market_hash_name):
     base_url = "https://steamcommunity.com/market/priceoverview/"
     params = {
         'country': 'CA',
@@ -21,23 +21,47 @@ def get_steam_market_price(app_id, market_hash_name):
     except requests.RequestException as e:
         print(f"Error: {e}")
         return None
+    
+def get_steam_market_price_lira(app_id, market_hash_name):
+    base_url = "https://steamcommunity.com/market/priceoverview/"
+    params = {
+        'country': 'TR',
+        'currency': 17,
+        'appid': app_id,
+        'market_hash_name': market_hash_name
+    }
+    
+    try:
+        response = requests.get(base_url, params=params, timeout=10)
+        response.raise_for_status()
+        
+        data = response.json()
+        if data.get('success'):
+            return data
+        else:
+            return None
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        return None
 
-def display_price_info(market_hash_name, price_data, exchange_rate, skinport_price_cad):
+def display_price_info(market_hash_name, cad_price_data, lira_price_data, skinport_price_cad):
     item_name = market_hash_name
-    lowest_price_cad_value = float(price_data.get('lowest_price', 'N/A').replace("CDN$ ", "")) if price_data.get('lowest_price', 'N/A') != 'N/A' else None
-    median_price_cad_value = float(price_data.get('median_price', 'N/A').replace("CDN$ ", "")) if price_data.get('lowest_price', 'N/A') != 'N/A' else None
 
-    # Convert CAD prices to Lira
-    lowest_price_lira = lowest_price_cad_value * exchange_rate if lowest_price_cad_value is not None else None
-    median_price_lira = median_price_cad_value * exchange_rate if median_price_cad_value is not None else None
+    # Get floats of cad values
+    lowest_price_cad_value = float(cad_price_data.get('lowest_price', 'N/A').replace("CDN$ ", "")) if cad_price_data.get('lowest_price', 'N/A') != 'N/A' else None
+    median_price_cad_value = float(cad_price_data.get('median_price', 'N/A').replace("CDN$ ", "")) if cad_price_data.get('lowest_price', 'N/A') != 'N/A' else None
+
+    # Get floats of lira values
+    lowest_price_lira_value = float(lira_price_data.get('lowest_price', 'N/A').replace(" TL", "").replace("," , ".")) if lira_price_data.get('lowest_price', 'N/A') != 'N/A' else None
+    median_price_lira_value = float(lira_price_data.get('median_price', 'N/A').replace(" TL", "").replace("," , ".")) if lira_price_data.get('lowest_price', 'N/A') != 'N/A' else None
 
     # Adjusted calculations for lowest price
-    adjusted_lowest_price_cad = lowest_price_cad_value / 1.15 if lowest_price_cad_value is not None else None
+    adjusted_lowest_price_cad = lowest_price_cad_value / 1.15 - 0.01 if lowest_price_cad_value is not None else None
     diff_lowest = adjusted_lowest_price_cad - skinport_price_cad if adjusted_lowest_price_cad is not None else None
     ratio_lowest = adjusted_lowest_price_cad / skinport_price_cad if adjusted_lowest_price_cad is not None else None
 
     # Adjusted calculations for median price
-    adjusted_median_price_cad = median_price_cad_value / 1.15 if median_price_cad_value is not None else None
+    adjusted_median_price_cad = median_price_cad_value / 1.15 - 0.01 if median_price_cad_value is not None else None
     diff_median = adjusted_median_price_cad - skinport_price_cad if adjusted_median_price_cad is not None else None
     ratio_median = adjusted_median_price_cad / skinport_price_cad if adjusted_median_price_cad is not None else None
 
@@ -54,7 +78,7 @@ def display_price_info(market_hash_name, price_data, exchange_rate, skinport_pri
 
     # Steam Lowest Price details
     print("Steam Lowest Price Details".center(50))
-    print(f"{'Price (Lira)':<40}: {lowest_price_lira:.2f}₺")
+    print(f"{'Price (Lira)':<40}: {lowest_price_lira_value:.2f}₺")
     print(f"{'Price (CAD)':<40}: ${lowest_price_cad_value:.2f}")
     print(f"{'Adjusted Price (CAD)':<40}: ${adjusted_lowest_price_cad:.2f}")
     print(f"{'Ratio':<40}: {ratio_lowest:.2f}")
@@ -63,7 +87,7 @@ def display_price_info(market_hash_name, price_data, exchange_rate, skinport_pri
 
     # Steam Median Price details
     print("Steam Median Price Details".center(50))
-    print(f"{'Price (Lira)':<40}: {median_price_lira:.2f}₺" if median_price_lira is not None else "N/A")
+    print(f"{'Price (Lira)':<40}: {median_price_lira_value:.2f}₺" if median_price_lira_value is not None else "N/A")
     print(f"{'Price (CAD)':<40}: ${median_price_cad_value:.2f}" if median_price_cad_value is not None else "N/A")
     print(f"{'Adjusted Price (CAD)':<40}: ${adjusted_median_price_cad:.2f}" if adjusted_median_price_cad is not None else "N/A")
     print(f"{'Ratio':<40}: {ratio_median:.2f}" if ratio_median is not None else "N/A")
@@ -71,7 +95,7 @@ def display_price_info(market_hash_name, price_data, exchange_rate, skinport_pri
     print(divider)
 
     # Volume
-    print(f"{'Volume':<40}: {price_data.get('volume', 'N/A')}")
+    print(f"{'Volume':<40}: {cad_price_data.get('volume', 'N/A')}")
     print(divider)
 
 if __name__ == "__main__":
@@ -97,11 +121,11 @@ if __name__ == "__main__":
         print("Invalid input. Please enter a valid price in format: CA$59.71")
         exit()
 
-    exchange_rate = 19.99  # CAD to Lira
+    cad_price = get_steam_market_price_cad(app_id, market_hash_name)
+    lira_price = get_steam_market_price_lira(app_id, market_hash_name)
 
-    price = get_steam_market_price(app_id, market_hash_name)
-    if price:
-        display_price_info(market_hash_name, price, exchange_rate, skinport_price_cad)
+    if cad_price and lira_price:
+        display_price_info(market_hash_name, cad_price, lira_price, skinport_price_cad)
     else:
         print("Couldn't fetch the price.")
 
